c8a70043b58f329e36c694bd6b757f1306460547
Brave Leuterio <you@example.com>
2020-08-22 06:19:56 +0800

    added mock db in region and summary  db test

--- test/region_database_test.dart
+++ test/region_database_test.dart
@@ -1,22 +1,37 @@
+import 'dart:convert';
+
 import 'package:Covid19_PH/model/base/jsonBase.dart';
-import 'package:Covid19_PH/model/record_list.dart';
+import 'package:Covid19_PH/model/region.dart';
 import 'package:Covid19_PH/model/region_list.dart';
 import 'package:Covid19_PH/services/region_database.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class MockDatabase extends Mock implements RegionDatabase {}
 
 void main() {
-  group("Testing Record Database", () {
-    RegionDatabase recordDatabase;
-    RegionList expectedOutput;
+  MockDatabase mockDatabase;
+  JsonBase expectedOutput;
+  group("Testing Region Database", () {
     setUp(() {
-      recordDatabase = RegionDatabase.instance;
-      expectedOutput = RegionList(regionList: [], lastUpdate: 'no-date');
+      final model = RegionList();
+      mockDatabase = MockDatabase();
+      expectedOutput = RegionList(regionList: [
+        Region(region: 'region xii: soccsksargen', cases: 551),
+        Region(region: 'caraga', cases: 370),
+      ], lastUpdate: "8/21/2020, 9:38:13 PM");
+
+      final decodedJson = json.decode(
+          '{"data":[{"region":"region xii: soccsksargen","cases":551},{"region":"caraga","cases":370}],"last_update":"8/21/2020, 9:38:13 PM"}');
+
+      when(mockDatabase.getTopRegions())
+          .thenAnswer((_) async => model.fromJson(decodedJson));
     });
 
-    test("Method Test: fetchRecord()", () async {
-      JsonBase item = await recordDatabase.getTopRegions();
-      RecordList itemData = item.getData();
-      expect(itemData.runtimeType, expectedOutput.runtimeType);
+    test("Method Test: getTopRegions()", () async {
+      final item = await mockDatabase.getTopRegions();
+      final itemData = item.getData();
+      expect(itemData, expectedOutput);
     });
   });
 }
