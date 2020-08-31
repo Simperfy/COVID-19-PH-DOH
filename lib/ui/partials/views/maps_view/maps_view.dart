9b1ad19bb396a5ba26a6bffbb8924d2c446c0155
Simperfy <28738855+Simperfy@users.noreply.github.com>
2020-08-31 13:27:31 +0800

    ADDED: CaseCoordinates

--- lib/ui/partials/views/maps_view/maps_view.dart
+++ lib/ui/partials/views/maps_view/maps_view.dart
@@ -1,8 +1,16 @@
+import 'package:Covid19_PH/ui/partials/views/maps_view/_CaseCoordinates.dart';
 import 'package:flutter/material.dart';
 import 'package:latlong/latlong.dart';
 import 'package:flutter_map/flutter_map.dart';
 
 class MapsView extends StatelessWidget {
+  final List<CaseCoordinates> coordinates = [
+    CaseCoordinates(cases: 1000, latitude: 13.019, longitude: 122.767),
+    CaseCoordinates(cases: 100, latitude: 11.019, longitude: 122.167),
+    CaseCoordinates(cases: 50, latitude: 14.019, longitude: 122.467),
+    CaseCoordinates(cases: 10, latitude: 15.019, longitude: 122.367),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return FlutterMap(
@@ -15,34 +23,33 @@ class MapsView extends StatelessWidget {
             urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
             subdomains: ['a', 'b', 'c']),
         MarkerLayerOptions(
-          markers: [
-            Marker(
-              width: 30.0,
-              height: 30.0,
-              point: LatLng(13.019, 122.767),
-              builder: (ctx) => Container(
-                child: Container(width: 80.0, height: 80.0, decoration: BoxDecoration(backgroundBlendMode: BlendMode.colorBurn,shape: BoxShape.circle, color: Colors.red),),
-              ),
-            ),
-            Marker(
-              width: 30.0,
-              height: 30.0,
-              point: LatLng(11.019, 121.767),
-              builder: (ctx) => Container(
-                child: Container(width: 80.0, height: 80.0, decoration: BoxDecoration(backgroundBlendMode: BlendMode.colorBurn,shape: BoxShape.circle, color: Colors.red),),
-              ),
-            ),
-            Marker(
-              width: 40.0,
-              height: 40.0,
-              point: LatLng(14.019, 121.767),
-              builder: (ctx) => Container(
-                child: Container(width: 80.0, height: 80.0, decoration: BoxDecoration(backgroundBlendMode: BlendMode.colorBurn,shape: BoxShape.circle, color: Colors.red),),
-              ),
-            ),
-          ],
+          markers: _buildMarkers(coordinates),
         ),
       ],
     );
+  }
+
+  List<Marker> _buildMarkers(List<CaseCoordinates> coordinates) {
+    List<Marker> res = [];
+
+    coordinates.forEach((coordinate) {
+      res.add(Marker(
+        width: 30.0,
+        height: 30.0,
+        point: LatLng(coordinate.latitude, coordinate.longitude),
+        builder: (ctx) => Container(
+          child: Container(
+            width: 80.0,
+            height: 80.0,
+            decoration: BoxDecoration(
+                backgroundBlendMode: BlendMode.colorBurn,
+                shape: BoxShape.circle,
+                color: Colors.red),
+          ),
+        ),
+      ));
+    });
+
+    return res;
   }
 }
